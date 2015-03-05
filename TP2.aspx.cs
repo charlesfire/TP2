81237a0f375476f7c9a2a56d4befa956d78bf360
Charlesfire <charlesfire2@hotmail.com>
2015-03-05 15:07:00 -0500

    Base de code

--- TP2.aspx.cs
+++ TP2.aspx.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class TP2 : System.Web.UI.Page
 {
+  #region Charles
     protected void Page_Load(object sender, EventArgs e)
     {
-
+      
     }
+  #endregion
+
+  #region Vincent
+
+  #endregion
 }
